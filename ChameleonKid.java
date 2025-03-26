ea162991d60a928bfece956a9b83dbab0792ec1c
danielji123 <dji224@student.fuhsd.org>
2025-03-26 13:50:35 -0700

    Create ChameleonKid.java

---
+++ ChameleonKid.java
@@ -0,0 +1,55 @@
+import info.gridworld.actor.Actor;
+import info.gridworld.actor.Critter;
+import info.gridworld.actor.Flower;
+import info.gridworld.grid.Location;
+import info.gridworld.grid.Grid;
+import java.util.ArrayList;
+/**
+ * A ChameleonKid copies the color of neighboring actors
+ * that are in front or behind as it moves through the grid. 
+ * @author Daniel Ji
+ */
+public class ChameleonKid extends ChameleonCritter
+{
+	 /**
+	 * Gets the actors for processing. The actors must be contained in the
+	 * same grid as this critter. Implemented to return the actors that
+	 * occupy neighboring grid locations in front or behind this critter.
+	 * @return a list of actors that are neighbors of this critter
+	 */
+	 public ArrayList<Actor> getActors()
+	 {
+		 ArrayList<Actor> actors = new ArrayList<Actor>();
+		 // the two actors of interest are in front and 180 degrees
+		 int[] dirs =
+		 { Location.AHEAD, Location.HALF_CIRCLE }; 
+		 for (Location loc : getLocationsInDirections(dirs))
+		 {
+			 Actor a = getGrid().get(loc);
+			 if (a != null)
+				actors.add(a);
+		 }
+		 return actors;
+	 } 
+	 /**
+	 * Finds the valid adjacent locations of this critter in different
+	 * directions.
+	 * @param directions - an array of directions (which are relative to the
+	 * current direction)
+	 * @return a set of valid locations that are neighbors of the current
+	 * location in the given directions
+	 */
+	 public ArrayList<Location> getLocationsInDirections(int[] directions)
+	 {
+		 ArrayList<Location> locs = new ArrayList<Location>();
+		 Grid gr = getGrid();
+		 Location loc = getLocation();
+		 for (int d : directions)
+		 {
+			 Location neighborLoc = loc.getAdjacentLocation(getDirection() + d);
+			 // any valid neighbors will be put into the list
+			 if(gr.isValid(neighborLoc)) locs.add(neighborLoc);
+		 }
+		 return locs;
+	}
+} 
